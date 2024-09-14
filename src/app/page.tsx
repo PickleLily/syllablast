85a965b3696a07c182d05335cf30e756e09d52d3
PickleLily <lily.bromberger@gmail.com>
2024-09-13 20:09:24 -0400

    video 3 initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+  <main className="flex flex-col gap-8 row-start-2 items-center justify-between p-24 sm:items-start">
+   <h1>Hello, World!</h1>
+   </main>
+  );
+}
---
+++ src/model.ts
@@ -0,0 +1,6 @@
+export class Syllable{
+
+}
+
+export class Puzzle{
+}
