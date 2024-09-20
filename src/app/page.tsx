abe2eb0c08933941e64cebc53232aff3636c01a5
PickleLily <lily.bromberger@gmail.com>
2024-09-20 19:21:52 -0400

    added starter code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+'use client'                              // directive to clarify client-side
+import React from 'react'
+
+import { config1, config2, config3 } from '../puzzle'
+import { Model } from '../model'
+import { BoardGUI } from '../boundary'
+import { realHandle } from '../controllers'
+
+var actualPuzzle = config1
+
+
 export default function Home() {
+  // initial instantiation of the Model comes from the actualPuzzle
+  const [model, setModel] = React.useState(new Model(actualPuzzle))
+  const [redraw, setRedraw] = React.useState(0)
+
+  function refresh() {
+    setRedraw(redraw+1)
+  }
+
+  function handleReset() {
+    realHandle(model, refresh)
+  }
+
   return (
-  <main className="flex flex-col gap-8 row-start-2 items-center justify-between p-24 sm:items-start">
-   <h1>Hello, World!</h1>
-   </main>
-  );
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <BoardGUI topmodel={model} redraw={refresh}/>
+      <button className="resetButton" onClick={() => handleReset()}>reset</button>
+    </div>
+  )
 }
--- src/model.ts
+++ src/model.ts
@@ -1,6 +1,43 @@
-export class Syllable{
+export class Coordinate {
+    readonly row : number;
+    readonly column : number;
+
+    constructor(row:number, column:number) {
+      this.row = row;
+      this.column = column;
+    }
+}
+
+export class Board {
+    syllables : string[][]
+
+    constructor() {
+        this.syllables = []
+        for (let r:number = 0; r < 4; r++) {
+            this.syllables[r] = []
+            for (let c:number = 0; c < 4; c++) {
+                this.syllables[r][c] = ''
+            }
+        }
+    }
 
 }
 
-export class Puzzle{
+export class Model {
+    words : string[]
+    board : Board
+
+    constructor(puzzle) {
+
+        this.words = [ "in,vis,i,ble" , "im,mac,u,late", "af,fil,i,ate", "un,der,wa,ter" ]
+
+        let board = new Board()
+        for (let r:number = 0; r < 4; r++) {
+            for (let c:number = 0; c < 4; c++) {
+                board.syllables[r][c] = puzzle.initial[r][c]
+            }
+        }
+        this.board = board
+    }
+
 }
