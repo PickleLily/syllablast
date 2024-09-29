4258eb0b57c77a1fb860733800fae7fbbd0abedd
PickleLily <lily.bromberger@gmail.com>
2024-09-29 18:31:27 -0400

    fixed the reset button so it stays on the correct configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 'use client'                              // directive to clarify client-side
 import React from 'react'
-import { config1, config2, config3 } from '../configurationInfo'
+import { configuration, config1, config2, config3 } from '../configurationInfo'
 import { Model } from '../model'
 import { BoardGUI } from '../boundary'
 
 var chosenConfig = config1
 
 export default function Home() {
-
-	var chosenConfig = config1
   
   // initial instantiation of the Model comes from the chosenPuzzle
   const [model, setModel] = React.useState(new Model(chosenConfig))
@@ -33,6 +31,12 @@ export default function Home() {
 	setRedraw(redraw+1)
   }
 
+function changeConfig(configuration : configuration){
+	chosenConfig = configuration
+	setModel(new Model(configuration))
+}
+
+
   return (
 	<div className = "items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 		<h1 className="title"> Syllablast</h1>
@@ -42,13 +46,13 @@ export default function Home() {
 		<label className="numMoves">{"Number of Moves: " + model.numMoves}</label>
 		<label className="pts">{"Points: " + model.points}</label>
 
-		<button className="button resetButton" onClick={() => setModel(new Model(chosenConfig))}>Reset</button>
+		<button className="button resetButton" onClick={() => changeConfig(chosenConfig)}>Reset</button>
 		<button className="button undoButton" onClick={() => handleUndo(model)}>Undo</button>
 		<button className="button swapButton" onClick={() => handleSwap(model)}>Swap</button>
 
-		<button className="button c1Button" onClick={() => setModel(new Model(config1))}>Config 1</button>
-		<button className="button c2Button" onClick={() => setModel(new Model(config2))}>Config 2</button>
-		<button className="button c3Button" onClick={() => setModel(new Model(config3))}>Config 3</button>
+		<button className="button c1Button" onClick={() => changeConfig(config1)}>Config 1</button>
+		<button className="button c2Button" onClick={() => changeConfig(config2)}>Config 2</button>
+		<button className="button c3Button" onClick={() => changeConfig(config3)}>Config 3</button>
 		
 	</div>
   )
