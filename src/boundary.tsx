39dea0a6248067ccb446619532a579ec01bdc849
PickleLily <lily.bromberger@gmail.com>
2024-09-30 10:33:39 -0400

    button no longer disabled

--- src/boundary.tsx
+++ src/boundary.tsx
@@ -1,63 +1,56 @@
 'use client'                              // directive to clarify client-side
 import React from 'react'
+import {useState} from 'react'
 import { Model } from './model'
 
-var mostRecentlyClicked = -1
+// var mostRecentlyClicked = -1
+// var disabled = useState
+
 
 export function BoardGUI({topmodel, redraw}) {
+  const [disabled, setDisabled] = useState(false);  // control for disabled state if needed
+  const [mostRecentlyClicked, setMostRecentlyClisked] = useState(-1);  // control for disabled state if needed
+
 
     function handleClick(r:number, c:number) {
-      console.log("clicked on: ", r, c, redraw)
       if(topmodel.board.sellectedSyllable1 == undefined){
         topmodel.board.sellectedSyllable1 = topmodel.board.syllables[r][c]
-        mostRecentlyClicked = 1
+        setMostRecentlyClisked(1)
 
       }else if (topmodel.board.sellectedSyllable2 == undefined){
         topmodel.board.sellectedSyllable2 = topmodel.board.syllables[r][c]
-        mostRecentlyClicked = 2
+        setMostRecentlyClisked(2)
 
       }else if(mostRecentlyClicked == 2){
         topmodel.board.sellectedSyllable1 = topmodel.board.syllables[r][c]
-        mostRecentlyClicked = 1
+        setMostRecentlyClisked(1)
 
       }else if (mostRecentlyClicked == 1){
         topmodel.board.sellectedSyllable2 = topmodel.board.syllables[r][c]
-        mostRecentlyClicked = 2
+        setMostRecentlyClisked(2)
       }
+      setDisabled(false)
       redraw(redraw + 1)  // call the redraw
     }
-    
+
     return (
       <>
         <div>
-          <button className="square" onClick={() => handleClick(0, 0)}>{topmodel.board.syllables[0][0].syllable}</button>
-          <button className="square" onClick={() => handleClick(0, 1)}>{topmodel.board.syllables[0][1].syllable}</button>
-          <button className="square" onClick={() => handleClick(0, 2)}>{topmodel.board.syllables[0][2].syllable}</button>
-          
-          {/* TODO -- only this one is not adding an X...
-          NOT REGISTERINGIT EVEN AS A BUTTON?? --> when ther is two it works if i click the most right one first THEN the one in the normal position...
-          */}
-          <button className="square" onClick={() => handleClick(0, 3)}>{topmodel.board.syllables[0][3].syllable}</button>        
-        </div>
-        <div>
-          <button className="square" onClick={() => handleClick(1, 0)}>{topmodel.board.syllables[1][0].syllable}</button>
-          <button className="square" onClick={() => handleClick(1, 1)}>{topmodel.board.syllables[1][1].syllable}</button>
-          <button className="square" onClick={() => handleClick(1, 2)}>{topmodel.board.syllables[1][2].syllable}</button>
-          <button className="square" onClick={() => handleClick(1, 3)}>{topmodel.board.syllables[1][3].syllable}</button>
-        </div>
-        <div>
-          <button className="square" onClick={() => handleClick(2, 0)}>{topmodel.board.syllables[2][0].syllable}</button>
-          <button className="square" onClick={() => handleClick(2, 1)}>{topmodel.board.syllables[2][1].syllable}</button>
-          <button className="square" onClick={() => handleClick(2, 2)}>{topmodel.board.syllables[2][2].syllable}</button>
-          <button className="square" onClick={() => handleClick(2, 3)}>{topmodel.board.syllables[2][3].syllable}</button>
-        </div>
-        <div>
-          <button className="square" onClick={() => handleClick(3, 0)}>{topmodel.board.syllables[3][0].syllable}</button>
-          <button className="square" onClick={() => handleClick(3, 1)}>{topmodel.board.syllables[3][1].syllable}</button>
-          <button className="square" onClick={() => handleClick(3, 2)}>{topmodel.board.syllables[3][2].syllable}</button>
-          <button className="square" onClick={() => handleClick(3, 3)}>{topmodel.board.syllables[3][3].syllable}</button>
+          {topmodel.board.syllables.map((row, rowIndex) => (
+            <div key={rowIndex}>
+              {row.map((syllable, colIndex) => (
+                <button
+                  className="square"
+                  disabled={false}  // manage disable logic here if needed
+                  onClick={() => handleClick(rowIndex, colIndex)}
+                >
+                  {syllable.syllable}
+                </button>
+              ))}
+            </div>
+          ))}
         </div>
       </>
-    )
+    );
   }
 
